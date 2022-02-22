ba8602a1b1f7185730c1303357c8808460a2de80
Amir Hamza Asif <amirasif999hamza000@gmail.com>
2022-02-22 18:17:03 +0600

    change

--- Convert one to another/DollarToTaka.js
+++ Convert one to another/DollarToTaka.js
@@ -1,6 +1,6 @@
 function convert(dollar) {
-    var taka = dollar*85;
+    var taka = dollar * 85;
     return taka;
 }
-var money = convert(500);
-console.log('dollar to taka', money)
+var money = convert(100);
+console.log(money, 'Taka')
